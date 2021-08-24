b813b5ecc1dae2a220ad5c135bead51b755d5a21
Miguel Angel Moreno <45338912+MiguelMorenoGit@users.noreply.github.com>
2021-08-24 20:58:14 +0200

    jump landing done

--- Ghost-Game/Assets/Scripts/Player Scripts/PlayerController.cs
+++ Ghost-Game/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -255,9 +255,7 @@ public class PlayerController : MonoBehaviour {
 
         ParticleSystem DustJump = Instantiate(DustJumpEffect, player.transform.position + new Vector3(0,0.1f,0), player.transform.rotation);
         DustJump.Play();
-        //asdasdasddsw
         
-
     }
 
 
